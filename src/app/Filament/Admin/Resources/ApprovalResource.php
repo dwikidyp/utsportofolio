d1bbb1abaa429c311655e1864ff72a9a0e09da04
dwikidyp <dwiki834@gmail.com>
2026-05-17 22:31:40 +0700

    first commit

---
+++ src/app/Filament/Admin/Resources/ApprovalResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Admin\Resources;
+
+use App\Filament\Admin\Resources\ApprovalResource\Pages;
+use App\Filament\Admin\Resources\ApprovalResource\RelationManagers;
+use App\Models\Approval;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ApprovalResource extends Resource
+{
+    protected static ?string $model = Approval::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListApprovals::route('/'),
+            'create' => Pages\CreateApproval::route('/create'),
+            'edit' => Pages\EditApproval::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ src/app/Filament/Admin/Resources/ApprovalResource/Pages/EditApproval.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Admin\Resources\ApprovalResource\Pages;
+
+use App\Filament\Admin\Resources\ApprovalResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditApproval extends EditRecord
+{
+    protected static string $resource = ApprovalResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
